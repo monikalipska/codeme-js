41db536ffee232f6f6fec43972a713fabb989a7d
hackathonjs <marcin.kaluzny@me.com>
2017-06-13 13:16:48 +0200

    Update zadaniedwieGwiazdki.js

--- 07062017-zadania/zadaniedwieGwiazdki.js
+++ 07062017-zadania/zadaniedwieGwiazdki.js
@@ -6,8 +6,12 @@
     sie w liscie Array lub z przedzialu okreslonego przez dwa parametry 
 */
 
+/*
+    nadal podkreslam wage styli kodowanie - ciezko sie czyta ten kod :(
+*/
 function liczbyPierwsze () {
   const args = arguments;
+  // mozna uzyc juz args
   const lnArgs = arguments.length;
   let outback = [];
 
@@ -20,16 +24,23 @@ function liczbyPierwsze () {
     return true;
   }
   
+  // czy to sprawdzenie daje nam informacje ze mamy array - 
+  // raczej tylko ze mamy podany jeden argument - 
+  // jak podam przy wywolaniu np (3)
+  // to walnie bledem
+  // porownanie scisle ===
   if (lnArgs == 1) {
+      // czy mozna uzyc innej metody ? Jezeli tak - jakiej?
     args[0].forEach(function (element, index) {
      if (pomocnicza(element)) {
         outback.push(element);
       }
-    })
+    }) // brak srednika moze prowadzic do powaznych bledow ;)
     return outback.join(",");
   }
-
-  else if (lnArgs == 2) {
+  
+  else if (lnArgs == 2) { // bardzo nieczytelne wyglada jak by else if bylo samodzielna instrukcja, brak scislego porownania
+      // czy ta konstrukcja else if jest potrzeban ?
     const minInt = args[0];
     const maxInt = args[1];
 
@@ -38,6 +49,8 @@ function liczbyPierwsze () {
         outback.push(i);
       }
     }
+
+    // dwa razy zwracasz to samo - celowo?
     return outback.join(",");
   }
 }
